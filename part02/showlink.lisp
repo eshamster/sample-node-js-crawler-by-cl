a8c78b843774f620f1c411522035dbaafdf13bdf
eshamster <hamgoostar@gmail.com>
2016-01-24 06:45:16 +0000

    Update showlink sample to show absolute urls

--- part02/showlink.lisp
+++ part02/showlink.lisp
@@ -12,20 +12,23 @@
 (defvar *dependencies* '("cheerio-httpcli")
   "Write dependent libralies of Node.js as string list")
 
-(defvar.ps client (require "cheerio-httpcli"))
+(defvar.ps *client* (require "cheerio-httpcli"))
+(defvar.ps *url* (require "url"))
 
 (defun main (&rest argv)
   (declare (ignorable argv))
   (with-use-ps-pack (:this)
     (let ((url "http://www.aozora.gr.jp/index_pages/person81.html")
           (param (make-hash-table)))
-      (client.fetch url param
-                    (lambda (err $ res)
-                      (when err
-                        (console.log "Error: " err)
-                        (return))
-                      (-- ($ "a")
-                          (each (lambda ()
-                                  (console.log (+ (-- ($ this) (text))
-                                                  ":"
-                                                  (-- ($ this) (attr "href"))))))))))))
+      (*client*.fetch url param
+                      (lambda (err $ res)
+                        (when err
+                          (console.log "Error: " err)
+                          (return))
+                        (-- ($ "a")
+                            (each (lambda ()
+                                    (let ((text (-- ($ this) (text)))
+                                          (href (-- ($ this) (attr "href"))))
+                                      (unless href (return))
+                                      (console.log (+ text ":" href))
+                                      (console.log (+ "  =>" (*url*.resolve url href))))))))))))
